c144d8fddb95c7643def4b467ec6dfa8f08c0ab1
RET Naphut <retnaphut@RETs-MacBook-Air.local>
2026-02-22 22:06:10 +0700

    Update API URL to new backend service (backend-ecomerce-shirt-4)

--- src/services/adminApi.ts
+++ src/services/adminApi.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'https://backend-ecomerce-shirt-4.onrender.com/api';
 
 // Interfaces
 export interface LoginResponse {
